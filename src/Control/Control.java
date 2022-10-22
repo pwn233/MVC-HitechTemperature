0a0ef27a9b078217af84a9bd6c09fcb86c67e30f
pwn233 <astiweiei@gmail.com>
2022-10-22 18:08:36 +0700

    fix explain
    remove thai language explain because not supported.

--- src/Control/Control.java
+++ src/Control/Control.java
@@ -13,26 +13,22 @@ import javax.swing.table.DefaultTableModel;
  * @author User
  */
 public class Control {
-    //เรียกใช้ model
+
     Model m = new Model();
     public void setStuID(String stuID) {
-        //เป็น method สำหรับให้ model set ค่า student id จาก gui(view)
+
         m.setStuID(stuID);
     }
     public void setTemperature(String temperature) {
-        //เป็น method สำหรับให้ model set ค่า temperature จาก gui(view)
+
         m.setTemperature(temperature);
     }
     public String process() {
-        //พาร์ทนี้เรียกการทำงานใน model ตามลำดับดังนี้
-        //เรียกอุณหภูมิที่ set ไว้
-        //นำอุณหภูมิไปเช็ค
-        //จดบันทึก log ลง database
+
         return m.insertDataDB(m.tempStatusCheck(m.getTemp()));
     }
 
     public DefaultTableModel showData(DefaultTableModel model) {
-        //เป็น method ในการเรียกดึงข้อมูล ใน database มาแสดงจาก model และ คืนค่ากลับไปให้แสดง
         m.showData(model);
         return model;
     }
--- src/Model/Model.java
+++ src/Model/Model.java
@@ -16,52 +16,35 @@ import javax.swing.table.DefaultTableModel;
  * @author User
  */
 public class Model {
-    //ประกาศตัวแปร stuID stand for student id มารองรับค่า 
-    //ประกาศตัวแปร temperatire มารองรับอุณหภูมิ
     private String stuID;
     private double temperature;
     
     public void setStuID(String stuID) {
-        //method สำหรับ set ค่า student ID
         this.stuID = stuID;
     }
     public double getTemp() {
-        //method สำหรับ คืนค่า temperature เมื่อถูกเรียกใช้
         return temperature;
     }
     public void setTemperature(String temperature) {
-        //method สำหรับ set ค่า temperature
         this.temperature = Double.parseDouble(temperature);
     }
     public String tempStatusCheck(double temperature) {
-        //method นี้เป็น algorithm based on การวัดอุณหภูมิตามที่โจทย์ require
-        //สร้าง status มารองรับในการคืนค่า high low normal
         String statusTemp = "";
-        //เงื่อนไขแรกสุดคือการตรวจอุณหภูมิสูงกว่ากฏ โดยจะคืนค่าเป็น high
         if(temperature > 37.5)
             statusTemp = "high";
-        //เงื่อนไขต่อมาคือการตรวจอุณหภูมิต่ำกว่ากฏ โดยจะคืนค่าเป็น low
         else if(temperature < 35)
             statusTemp = "low";
         else
-        //เงื่อนไขท้ายสุดคือการตรวจอุณหภูมิที่ไม่เข้าข่ายตามกฏ โดยจะคืนค่าเป็น normal  
             statusTemp = "normal";
-        //คืนค่า สถานะ high low normal กลับไป
         return statusTemp;
     }
     
     public String insertDataDB(String statusTemp) {
-        //เป็น method เพื่อ insert ข้อมูลที่มีลงบันทึกเป็น log ใน database
-        //สร้าง query ไว้รองรับคำสั่ง mysql
         String query = "";
-        //สร้าง status ไว้เก็บสถานะของการใช้คำสั่ง mysql ว่าสำเร็จหรือไม่
         String status = "";
-        //ใช้ java lib date time formatter ในการเรียกใช้วันเวลา และ local date time ในการเรียกช่วงเวลาปัจจุบัน
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
         LocalDateTime now = LocalDateTime.now();  
-        //เก็บค่าวันเวลาลงในรูปแบบของ string(in mysql : varchar)
         String date = dtf.format(now);
-        //เปลี่ยนรูปแบบทศนิยมเป็น หลักเดียว
         String pattern = "##.#";
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
         String tempDec = decimalFormat.format(temperature);
@@ -83,7 +66,6 @@ public class Model {
     }
 
     public DefaultTableModel showData(DefaultTableModel model) {
-        //method ดึงข้อมูลมาเป็นรูปแบบแถวเพื่อ port ลงตารางใน method ที่เรียก method นี้มา
         String query = "";
         try {
             DBConnect conn = new DBConnect();
